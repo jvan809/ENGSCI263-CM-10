44d028c130a00adcf031d625e980902c80d020c3
tcsi800 <tcsi800@aucklanduni.ac.nz>
2021-08-20 14:48:51 +1200

    accidentally submitted the wrong changes

--- model_fitting/cm_solver_functions.py
+++ model_fitting/cm_solver_functions.py
@@ -61,7 +61,7 @@ def model(t, X, q_stm, q_out, Tstm, aP, bP, P0, M0, T0, bT):
     return dXdt
 
 
-def solve_ode_scipy(model, t0, t1, Pi, Ti, pars, time_eval = None):
+def ode_solve(model, t0, t1, Pi, Ti, pars, time_eval = None):
     """
     Finds the solution to the model differential equation
     
@@ -116,39 +116,41 @@ def solve_ode_scipy(model, t0, t1, Pi, Ti, pars, time_eval = None):
 
 
 
-def solve_ode(f, t0, t1, dt, x0, pars, q=None):
-    ''' Solve an ODE numerically.
+##############################################################################################################
+'''This is the improved euler solver from the lab which we can use if the above funciton doesn't work'''
+# def solve_ode(f, t0, t1, dt, x0, pars, q=None):
+#     ''' Solve an ODE numerically.
 
-        Parameters:
-        -----------
-        f : callable
-            Function that returns dxdt given variable and parameter inputs.
-        t0 : float
-            Initial time of solution.
-        t1 : float
-            Final time of solution.
-        dt : float
-            Time step length.
-        x0 : float
-            Initial value of solution.
-        pars : array-like
-            List of parameters passed to ODE function f.
-        q : callable
-            function giving the flow at time=t
+#         Parameters:
+#         -----------
+#         f : callable
+#             Function that returns dxdt given variable and parameter inputs.
+#         t0 : float
+#             Initial time of solution.
+#         t1 : float
+#             Final time of solution.
+#         dt : float
+#             Time step length.
+#         x0 : float
+#             Initial value of solution.
+#         pars : array-like
+#             List of parameters passed to ODE function f.
+#         q : callable
+#             function giving the flow at time=t
 
-    '''
-    t = np.arange(t0, t1, dt) # create array of times
-    x = np.zeros(len(t)) # initialise solution output
-    x[0] = x0 # set initial value
+#     '''
+#     t = np.arange(t0, t1, dt) # create array of times
+#     x = np.zeros(len(t)) # initialise solution output
+#     x[0] = x0 # set initial value
     
-    if q is None: q = -np.ones(len(t)) # this function gives the value of q at time t
+#     if q is None: q = -np.ones(len(t)) # this function gives the value of q at time t
 
-    for i in range(len(t)-1):
-        k1 = f(t[i], x[i], q[i], *pars)            # /\
-        k2 = f(t[i+1], x[i] + k1*dt, q[i+1], *pars) # equations for the improved Euler method
-        x[i+1] = x[i] + dt*(k1+k2)/2                      # \/
-    return t,x
-
+#     for i in range(len(t)-1):
+#         k1 = f(t[i], x[i], q[i], *pars)            # /\
+#         k2 = f(t[i+1], x[i] + k1*dt, q[i+1], *pars) # equations for the improved Euler method
+#         x[i+1] = x[i] + dt*(k1+k2)/2                      # \/
+#     return t,x
+##############################################################################################################
 
 if __name__ == "__main__":
     print(model([1,1],1 ,1,1,1,1,1,1,1,1,1))
